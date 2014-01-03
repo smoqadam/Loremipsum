5a6db6c3c57a0da1fa904e724750688831cca24c
PHProir <phpro.ir@gmail.com>
2014-01-03 17:05:55 +0330

    Update and rename Lurem.php to Lorem.php

--- Lorem.php
+++ Lorem.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Create Lurem ipsum text in Persian and English language
+ * Create Lorem ipsum text in Persian and English language
  * 
  * How to use ?
- * You can create Lurem ipsum text in php apps by call Lurem::create() method :
- *      Lurem::create(Lurem::LANG_FA , $count = 10 , Lurem::TYPE_WORD , $end = '...'); 
+ * You can create Lorem ipsum text in php apps by call Lorem::create() method :
+ *      Lorem::create(Lorem::LANG_FA , $count = 10 , Lorem::TYPE_WORD , $end = '...'); 
  *
  * @author     Saeed Moghadam zade <phpro.ir@gmail.com>
  * @copyright  2014-2014 PHPro.ir
@@ -13,7 +13,7 @@
  * @link       http://phpro.ir/
  */
 
-class Lurem {
+class Lorem {
    
     /**
      * create text by word count
@@ -31,19 +31,15 @@ class Lurem {
     const TYPE_PARAGRAPH = 3;
 
     /**
-     * set lurem ipsum language to English
+     * set lorem ipsum language to English
      */
     const LANG_EN = 'en';
 
     /**
-     * set lurem ipsum language to Farsi
+     * set lorem ipsum language to Farsi
      */
     const LANG_FA = 'fa';
 
-    
-    
-    private $lang = 'fa';
-    
     
     static private $en = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.  ';
     static private $fa = 'لورم ایپسوم متنی است که ساختگی برای طراحی و چاپ آن مورد است. صنعت چاپ زمانی لازم بود شرایطی شما باید فکر ثبت نام و طراحی، لازمه خروج می باشد. در ضمن قاعده همفکری ها جوابگوی سئوالات زیاد شاید باشد، آنچنان که لازم بود طراحی گرافیکی خوب بود. کتابهای زیادی شرایط سخت ، دشوار و کمی در سالهای دور لازم است. هدف از این نسخه فرهنگ پس از آن و دستاوردهای خوب شاید باشد. حروفچینی لازم در شرایط فعلی لازمه تکنولوژی بود که گذشته، حال و آینده را شامل گردد. سی و پنج درصد از طراحان در قرن پانزدهم میبایست پرینتر در ستون و سطر حروف لازم است، بلکه شناخت این ابزار گاه اساسا بدون هدف بود و سئوالهای زیادی در گذشته بوجود می آید، تنها لازمه آن بود.  ';
@@ -53,11 +49,11 @@ class Lurem {
      * ساخت متن لورم ایپسوم 
      * 
      * 
-     * <pre> echo Lurem::create();</pre>
+     * <pre> echo Lorem::create();</pre>
      * 
-     * @param Lurem::LANG_FA $lang انتخاب زبان 
+     * @param Lorem::LANG_FA $lang انتخاب زبان 
      * @param int $count تعداد 
-     * @param Lurem::TYPE_WORD $type نوع خروجی
+     * @param Lorem::TYPE_WORD $type نوع خروجی
      * @param String $end اضافه کردن این کاراکتر به انتهای منت خروجی
      * @return String
      */
@@ -65,13 +61,13 @@ class Lurem {
         $out = '';
 
         if ($lang == self::LANG_FA)
-            $lurem = self::$fa;
+            $lorem = self::$fa;
         else
-            $lurem = self::$en;
+            $lorem = self::$en;
 
         switch ($type) {
             case self::TYPE_WORD :
-                $words = explode(' ', $lurem);
+                $words = explode(' ', $lorem);
                 $i = 0;
                 foreach ($words as $word) {
                     if ($i == $count)
@@ -82,12 +78,12 @@ class Lurem {
                 break;
             case self::TYPE_CHAR :
                 for ($i = 0; $i <= $count; $i++) {
-                    $out .= $lurem[$i];
+                    $out .= $lorem[$i];
                 }
                 break;
             case self::TYPE_PARAGRAPH:
                 for ($i = 0; $i < $count; $i++) {
-                    $out .= '<p>' . $lurem . '</p>';
+                    $out .= '<p>' . $lorem . '</p>';
                 }
                 break;
         }
